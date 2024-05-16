2ee5b1dd6ffc4c8b9b187c251162761d3c291eca
Rakesh Singh <36115281+RakeshSingh12@users.noreply.github.com>
2024-05-16 23:50:19 +0530

    TS vs JS diff added

---
+++ TS-vs-JS.js
@@ -0,0 +1,18 @@
+/* Difference between TypeScript and JavaScript */
+
+/* 
+    Typescript is a superset of javascript    
+ 
+   -> TypeScript is known as an Object-oriented programming language 
+   -> whereas JavaScript is a prototype-based language.
+
+   -> TypeScript has a feature known as Static typing 
+   -> JavaScript does not support this feature
+
+   -> TypeScript supports Interfaces but JavaScript does not.
+
+    -> Error can be found during compile-time
+    -> Error can be found  only  during run-time
+
+
+*/
